7e3fc37d9c87dcb9d5d36c9c5017dd4d1f788f3c
Avner José Guimarães Ribeiro <avnerjose@pop-os.localdomain>
2021-06-14 22:34:35 -0300

    Adjusting pwa

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPwa = require('next-pwa');
 module.exports = withPwa({
     pwa: {
         dest: 'public',
-        register: true
+        register: true,
+        disabled: process.env.NODE_ENV === 'development'
     }
 });
